b7a2952fb545b9f75abbdd4a49cbc35a3d005136
David Claros <davidalejo32@hotmail.com>
2023-05-09 16:03:21 -0500

    V 0.0.3

---
+++ src/controllers/EspecialidadController.ts
@@ -0,0 +1,58 @@
+import { PrismaClient } from '@prisma/client'
+import { Request, Response } from 'express'
+
+
+class EspecialidadController {
+
+	prismaCliente : PrismaClient
+
+	constructor(){
+		this.prismaCliente = new PrismaClient()
+	}
+
+	async listarEspecialidades(req: Request, res:Response) {
+		try {
+			const especialidades = await this.prismaCliente.especialidad.findMany()
+			res.json(especialidades)
+		} catch (error:any) {
+			res.json({error: error.message})
+		}
+	}
+
+	async listarEspecialidad(req: Request, res: Response){
+		try {
+
+			const { id } = req.params
+			const especialidad = await this.prismaCliente.especialidad.findUnique({
+				where:{
+					idEspecialidad: parseInt(id)
+				}
+			})
+
+			res.json(especialidad)
+		} catch (error:any) {
+			res.json({error: error.message})
+		}
+	}
+
+	async crearEspecialidad (req: Request, res: Response){
+		try {
+			const {nombre} = req.body
+
+			const especialidad = await this.prismaCliente.especialidad.create({
+				data: {
+					nombre
+				}
+			})
+
+			res.json(especialidad)
+
+		} catch (error:any) {
+			res.json({error: error.message})
+      
+		}
+	}
+
+}
+
+export default EspecialidadController
