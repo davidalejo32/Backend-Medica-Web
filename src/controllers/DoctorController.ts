2625781ca8124d0cb017f5da84b2a13bc86ce2f7
David Claros <davidalejo32@hotmail.com>
2023-05-09 16:03:58 -0500

    V 0.0.4

---
+++ src/controllers/DoctorController.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from 'express'
+import { PrismaClient }  from '@prisma/client'
+
+class DoctorController {
+	private prismaClient : PrismaClient
+
+	constructor(){
+		this.prismaClient = new PrismaClient()
+	}
+
+
+	async listarDoctores (req: Request, res:Response) {
+		try {
+			const doctores = await this.prismaClient.doctor.findMany()
+			res.json(doctores)
+		} catch (error:any) {
+			res.json({error: error.message})
+		}
+	}
+
+	async crearDoctor (req: Request, res: Response) {
+		try {
+			const { nombre, apellido, consultorio, correo, idEspecialidad } = req.body 
+
+			const doctor = await this.prismaClient.doctor.create(
+				{
+					data: {
+						nombre, 
+						apellido, 
+						consultorio, 
+						correo,
+						idEspecialidad: parseInt(idEspecialidad)
+					}
+				}
+			)
+
+			res.json(doctor)
+
+		} catch (error:any) {
+			res.json({error: error.message})
+		}
+	}
+
+}
+
+export default DoctorController
---
+++ src/routes/Doctor.routes.ts
@@ -0,0 +1,32 @@
+import DoctorController from '../controllers/DoctorController'
+import { Router, Request, Response } from 'express'
+
+
+class DoctorRouter {
+
+	router : Router
+	doctorController :  DoctorController
+
+	constructor() {
+		this.router = Router()
+		this.doctorController = new DoctorController()
+		this.routes()
+	}
+
+
+	private routes(): void {
+
+		this.router.get('/doctores', (req: Request, res: Response)=>{
+			this.doctorController.listarDoctores(req, res)
+		})
+    
+
+		this.router.post('/crear_doctor', (req: Request, res: Response)=>{
+			this.doctorController.crearDoctor(req, res)
+		})
+
+	}
+
+}
+
+export default DoctorRouter
